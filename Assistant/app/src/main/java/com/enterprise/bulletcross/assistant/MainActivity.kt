0457637ca64bcae6d5d2d90a6c13e40153540184
bulletcross <bulletcross@gmail.com>
2018-01-13 23:54:09 +0530

    added design flow

--- Assistant/app/src/main/java/com/enterprise/bulletcross/assistant/MainActivity.kt
+++ Assistant/app/src/main/java/com/enterprise/bulletcross/assistant/MainActivity.kt
@@ -3,6 +3,25 @@ package com.enterprise.bulletcross.assistant
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
+/**
+ * Service class constantly seeks for importance detection
+ * Once importance is detected, service changes state
+ * and constanly seeks for vicinity detection.
+ * Vicinity is detected until interacted.
+ * State 0 -> importance detection state (default)
+ * State 1 -> Vicinity detection state
+ * State 2 -> No detection state
+ * State 3 -> Notify state
+ *
+ * State Change Flow
+ * State 0 -> [interaction] -> State 2
+ * State 0 -> [no interaction + importance is true] -> State 1
+ * State 1 -> [interaction] -> State 2
+ * State 1 -> [no interaction + vicinity is true] -> State 3
+ * State 2 -> [no interaction] -> State 0
+ * State 3 -> [interaction] -> State 0
+ */
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ Assistant/app/src/main/java/com/enterprise/bulletcross/assistant/importance_detection.kt
@@ -0,0 +1,7 @@
+package com.enterprise.bulletcross.assistant
+
+/**
+ * Created by bulle on 13-01-2018.
+ */
+class importance_detection {
+}

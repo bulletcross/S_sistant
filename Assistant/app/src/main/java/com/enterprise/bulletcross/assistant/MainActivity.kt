a0754b2448389e1b5a5308cdafce09c34bb5b4ab
bulletcross <bulletcross@gmail.com>
2018-03-04 23:31:02 +0530

    graph view working

--- Assistant/app/src/main/java/com/enterprise/bulletcross/assistant/MainActivity.kt
+++ Assistant/app/src/main/java/com/enterprise/bulletcross/assistant/MainActivity.kt
@@ -7,8 +7,11 @@ import android.hardware.SensorManager
 import android.hardware.SensorEventListener
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
-
+import com.jjoe64.graphview.GraphView
+import com.jjoe64.graphview.series.DataPoint
+import com.jjoe64.graphview.series.LineGraphSeries
 
 /**
  * Service class constantly seeks for importance detection
@@ -34,12 +37,17 @@ class MainActivity : AppCompatActivity(),SensorEventListener {
     private var mSensorManager: SensorManager? = null
     private var mLight: Sensor? = null
 
+    private var graph:GraphView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         rgb_sensor_reading = findViewById(R.id.textView)
         mSensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
         mLight = mSensorManager!!.getDefaultSensor(Sensor.TYPE_LIGHT)
+        graph = findViewById<View>(R.id.graph) as GraphView
+        val series = LineGraphSeries<DataPoint>(arrayOf<DataPoint>(DataPoint(0.0, 1.0), DataPoint(1.0, 5.0), DataPoint(2.0, 3.0), DataPoint(3.0, 2.0), DataPoint(4.0, 6.0)))
+        graph!!.addSeries(series)
     }
 
     override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {
